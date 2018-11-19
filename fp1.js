3d4dab46b9ad0c7a257f2ca527e68570c7806b53
Natalia Proskurnicha <proskurnicha@gmail.com>
2018-11-19 20:56:32 +0200

    Added config for changing propeties

--- fp1.js
+++ fp1.js
@@ -1,16 +1,3 @@
-const input = [
-	{ name: 'TV', price: 300, date: '2018-10-10' },
-	{ name: 'laptop', price: 600, date: '2018-10-12' },
-	{ name: 'PC', price: 800, date: '2018-09-05' },
-	{ name: 'owen', price: 300 },
-	{ name: 'Camera', price: 500, date: '2018-03-03' },
-	{ name: 'Fridge', price: 1000, date: '2018-12-11' },
-	{ name: 'table', price: 150, date: '2018-12-10' },
-	{ name: 'Sofa', price: 400, date: '2018-12-08' },
-	{ name: 'chair', date: '2018-09-10' },
-	{ name: 'Window', price: 300, date: '2018-05-05' },
-];
-
 const firstLetterToUpperCase = ([firstLetter, ...rest]) => firstLetter.toUpperCase() + rest.join('');
 
 const plusDollar = value => value + '$';
@@ -25,24 +12,56 @@ function compare({date: d1}, {date: d2}) {
 
 const getSortedValuesByDate = (values, compare) => [...values].sort(compare);
 
-const changePropertiesElement = (element, changeName, changePrice, changeDate) => ({ 
-		name: changeName(element.name),
-		price: changePrice(element.price),
-		date: changeDate(element.date)
-	});
-
-const getResult = (values, changeName, changePrice, changeDate) => values.map((element) => changePropertiesElement(element, changeName, changePrice, changeDate));
-
-function devideOnTwoMassive(values) {
-	const output = values.filter(element => element.date && element.name && element.price);
-	const errors = values.filter(element => !(element.date && element.name && element.price));
-	const sorted = getSortedValuesByDate(output, compare);	
-	const result =  getResult(sorted, firstLetterToUpperCase, plusDollar, x => x+'p.m.');
-	console.log(result);	
-	// const res = pipe(
- //    	getResult(),
- //        getSortedValuesByDate()
-	// )(output);
+const mapItem(item, itemConfig) {
+	// todo: call fn for each props
 }
 
-devideOnTwoMassive(input);
+const changePropertiesElement = (element, {
+	name: nameMap,
+	price: priceMap,
+	date: dateMap
+}) => ({ 
+	name: nameMap(element.name),
+	price: priceMap(element.price),
+	date: dateMap(element.date)
+});	
+
+const map = (arr, fn, config) =>
+	arr.map(element => fn(element, config));
+
+const input = [
+	{ name: 'TV', price: 300, date: '2018-10-10' },
+	{ name: 'laptop', price: 600, date: '2018-10-12' },
+	{ name: 'PC', price: 800, date: '2018-09-05' },
+	{ name: 'owen', price: 300 },
+	{ name: 'Camera', price: 500, date: '2018-03-03' },
+	{ name: 'Fridge', price: 1000, date: '2018-12-11' },
+	{ name: 'table', price: 150, date: '2018-12-10' },
+	{ name: 'Sofa', price: 400, date: '2018-12-08' },
+	{ name: 'chair', date: '2018-09-10' },
+	{ name: 'Window', price: 300, date: '2018-05-05' },
+];
+
+const config = {
+	name: firstLetterToUpperCase,
+	price: plusDollar,
+	date: x => x
+}
+
+const isValid = element => element.date && element.name && element.price;
+
+const notValid = element => !isValid(element);
+
+const transform = (input, config) => {
+	const validatedArr = input.filter(isValid);
+	const errors = input.filter(notValid);
+	const sortedArr = [...validatedArr].sort(compare);	
+	const result = map(sortedArr, changePropertiesElement, config)
+	console.log(result);
+}
+
+transform(input, config);
+
+
+
+
